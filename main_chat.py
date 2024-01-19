344d36e64e8be9b76f2859aa35f400604e82993c
Ali Alkhars <a-khars@hotmail.com>
2024-01-19 13:42:19 +0000

    Add support for reberta and optimise T5 for question answering

--- main_chat.py
+++ main_chat.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoModelForCausalLM, AutoTokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoModelForCausalLM, AutoTokenizer, RobertaTokenizer, RobertaForQuestionAnswering
 
 class T5Chatbot:
     def __init__(self, model_name='t5-small'):
@@ -12,15 +12,29 @@ class T5Chatbot:
     def add_context(self, text):
         self.context += text + " "
 
-    def generate_response(self, input_text):
-        self.add_context(input_text)
-        input_ids = self.tokenizer.encode(self.context, return_tensors='pt').to(self.device)
+    def chat(self, question):
+        # Prepend with "question:" or "answer the question:"
+        input_text = f"question: {question} context: {self.context}"
+        input_ids = self.tokenizer.encode(input_text, return_tensors='pt').to(self.device)
+
+        # Update context for next question
+        self.add_context(question)
+
+        # Generate an answer
+        output_ids = self.model.generate(input_ids, max_length=150, num_beams=3, early_stopping=True)
+        answer = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+
+        return answer
+
+    # def generate_response(self, input_text):
+    #     self.add_context(input_text)
+    #     input_ids = self.tokenizer.encode(self.context, return_tensors='pt').to(self.device)
         
-        # Generate a response
-        output_ids = self.model.generate(input_ids, max_length=512, num_return_sequences=1, num_beams=5, temperature=0.9)
-        response = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
+    #     # Generate a response
+    #     output_ids = self.model.generate(input_ids, max_length=512, num_return_sequences=1, num_beams=5, temperature=0.9)
+    #     response = self.tokenizer.decode(output_ids[0], skip_special_tokens=True)
         
-        return response
+    #     return response
 
 class LlamaChatbot:
     def __init__(self, model_name="meta-llama/Llama-2-7b-chat-hf"):
@@ -42,25 +56,53 @@ class LlamaChatbot:
         response = self.tokenizer.decode(self.chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True)
         return response
 
+class RoBERTaChatbot:
+    def __init__(self):
+        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
+        self.model = RobertaForQuestionAnswering.from_pretrained('roberta-large')
+        self.context = ""
 
-choice = input("Choose a model (1 for T5), (2 for Llama-2): ")
+    def chat(self, question):
+        # Append the question to the context
+        input_text = self.context + " " + question
+        inputs = self.tokenizer(input_text, return_tensors='pt')
+
+        # Get model outputs
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        # Extract the answer
+        answer_start_scores, answer_end_scores = outputs.start_logits, outputs.end_logits
+        answer_start = torch.argmax(answer_start_scores)
+        answer_end = torch.argmax(answer_end_scores) + 1
+        answer = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(inputs['input_ids'][0][answer_start:answer_end]))
+
+        # Update the context
+        self.context += " " + question + " " + answer
+        return answer
+
+
+choice = input("Choose a model (1 for T5), (2 for Llama-2), (3 for RoBERTa): ")
 print()
 input_text = ''
+bot = None
 
 if choice == '1':
     bot = T5Chatbot()
-    while input_text != 'quit':
-        input_text = input("User: ")
-        response = bot.generate_response(input_text)
-        print(response, "\n\n")
+
 elif choice == '2':
     bot = LlamaChatbot()
-    while input_text != 'quit':
-        input_text = input("User: ")
-        response = bot.chat(input_text)
-        print(response, "\n\n")
+
+elif choice == '3':
+    bot = RoBERTaChatbot()
+    
 else:
     print("Incorrect input! Try again")
+
+while input_text != 'quit' and bot != None:
+    input_text = input("User: ")
+    response = bot.chat(input_text)
+    print("Career Bud: ", response, "\n\n")
 
 
 
