f5a4dd644dcedf0ab5975973b48bc974f87d6fd5
Ali Alkhars <a-khars@hotmail.com>
2024-01-30 12:38:37 +0000

    Move unused models

--- main_chat.py
+++ main_chat.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoModelForCausalLM, AutoTokenizer, RobertaTokenizer, RobertaForQuestionAnswering
+from transformers import T5ForConditionalGeneration, T5Tokenizer, AutoModelForCausalLM, AutoTokenizer
 
 class T5Chatbot:
     def __init__(self, model_name='t5-small'):
@@ -47,32 +47,6 @@ class LlamaChatbot:
     def name(self):
         return 'Llama-2'
 
-class RoBERTaChatbot:
-    def __init__(self):
-        self.tokenizer = RobertaTokenizer.from_pretrained('roberta-large')
-        self.model = RobertaForQuestionAnswering.from_pretrained('roberta-large')
-        self.fixed_context = "Ali is an Arab. He comes from Saudi Arabia. Ali is 22 years old, even though he feels 18."
-
-    def chat(self, question):
-        # Combine the fixed context with the question
-        input_text = self.fixed_context + " " + question
-        inputs = self.tokenizer(input_text, return_tensors='pt', truncation=True)
-
-        # Get model outputs
-        with torch.no_grad():
-            outputs = self.model(**inputs)
-
-        # Extract the answer
-        answer_start_scores, answer_end_scores = outputs.start_logits, outputs.end_logits
-        answer_start = torch.argmax(answer_start_scores)
-        answer_end = torch.argmax(answer_end_scores) + 1
-        answer = self.tokenizer.convert_tokens_to_string(self.tokenizer.convert_ids_to_tokens(inputs['input_ids'][0][answer_start:answer_end]))
-
-        return answer
-
-    def name(self):
-        return 'RoBERTa'
-
 class DialoGPTChatbot:
     def __init__(self, model_name="microsoft/DialoGPT-medium"):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='left')
@@ -106,35 +80,8 @@ class DialoGPTChatbot:
     def name(self):
         return 'DialoGPT'
 
-# Encounters error: RuntimeError: "LayerNormKernelImpl" not implemented for 'Half'
-class PhiChatbot:
-    def __init__(self, model_name="microsoft/phi-2"):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
 
-    def chat(self, user_input):
-        # inputs = self.tokenizer(user_input, return_tensors="pt", return_attention_mask=False)
-        # outputs = self.model.generate(**inputs, max_length=200)
-        # response = self.tokenizer.batch_decode(outputs)[0]
-
-        with torch.no_grad():
-            token_ids = self.tokenizer.encode(user_input, add_special_tokens=False ,return_tensors="pt")
-            output_ids = self.model.generate(
-                token_ids.to(self.device),
-                max_new_tokens=512,
-                do_sample=True,
-                temperature = 0.3
-            )
-
-        response = self.tokenizer.decode(output_ids[0][token_ids.size(1) :])
-        return response
-
-    def name(self):
-        return 'Phi-2'
-
-
-choice = input("Choose a model (1 for T5), (2 for DialoGPT), (3 for Llama-2), (4 for RoBERTa), (5 for Phi-2): ")
+choice = input("Choose a model (1 for T5), (2 for DialoGPT), (3 for Llama-2): ")
 print()
 input_text = ''
 bot = None
@@ -147,12 +94,6 @@ elif choice == '2':
 
 elif choice == '3':
     bot = LlamaChatbot()
-
-elif choice == '4':
-    bot = RoBERTaChatbot()
-
-elif choice == '5':
-    bot = PhiChatbot()
     
 else:
     print("Incorrect input! Try again")
