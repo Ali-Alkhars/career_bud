4f2708f92aba73b5ce523b87bc8fdeb984592237
Ali Alkhars <a-khars@hotmail.com>
2024-02-02 13:49:45 +0000

    Cleanse Essential JS questions

--- data_cleanser.py
+++ data_cleanser.py
@@ -43,7 +43,7 @@ def process_questions(input_file, output_file, regex, topic):
     for line in lines:
         match = pattern.match(line)
         if match:
-            question_text = match.group(0).strip()
+            question_text = match.group(1).strip()
             questions.append({
                 "topic": question_maker(topic),
                 "question": question_text
@@ -59,3 +59,4 @@ def process_questions(input_file, output_file, regex, topic):
 # process_questions('Datasets/Interviews Datasets/1000 top JS interview questions/Questions.md', 'interviews_dataset.json', r'\[(.*?)\]', "JavaScript")
 # process_questions('Datasets/Interviews Datasets/222 Java interview questions/Questions.md', 'interviews_dataset.json', r'- \d+ \. (.*)', "Java")
 # process_questions('Datasets/Interviews Datasets/Developer interview questions/Questions.md', 'interviews_dataset.json', r'^(?!#|\[).+$', "Back-end")
+process_questions('Datasets/Interviews Datasets/Essential JS interview questions/Questions.md', 'interviews_dataset.json', r'^## Question \d+\.\s*(.*)$', "JavaScript")
