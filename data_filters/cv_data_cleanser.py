7e110da32e60de840a41ccc49cc49d48641eb1a8
Ali Alkhars <a-khars@hotmail.com>
2024-02-20 18:10:04 -0500

    create cv dataset

---
+++ data_filters/cv_data_cleanser.py
@@ -0,0 +1,52 @@
+import json
+import random
+from datasets import load_dataset
+
+"""
+This script is intended to structure specific data files into 
+a Json dataset that will be used to train Language Models on 
+CV improvement tips.
+"""
+
+def question_maker():
+    """Return a random question from a pre-made list of questions"""
+    questions = [
+        f'Can you give me some tips to improve my CV?',
+        f'Can you give me some tips to improve my Resume?',
+        f'How can I improve my CV?',
+        f'How can I improve my Resume?',
+        f'I want ways to enhance my CV',
+        f'I want ways to enhance my Resume',
+        f'Could you suggest changes on me CV?',
+        f'Could you suggest changes on me Resume?',
+        f'Give me some CV tips',
+        f'Give me some Resume tips',
+        f'I need help with my CV',
+        f'I need help with my Resume',
+        f'I want you to help me update my CV',
+        f'I want you to help me update my Resume',
+        f'Could you improve my CV?',
+        f'Could you improve my Resume?',
+    ]
+    return random.choice(questions)
+
+def process_questions():
+    # extract the data
+    data = load_dataset('gkrishnan/Resume_Best_Practices')
+    text_data = data['train']['text'][0].replace('\n', ' ')  # Replacing \n with space
+
+    # Split the text into sentences (by full stop).
+    sentences = text_data.split('. ')
+    
+    # Group every two sentences together.
+    grouped_sentences = ['. '.join(sentences[i:i+2]) + '.' for i in range(0, len(sentences)-1, 2)]
+    
+    # Create the structured dataset.
+    structured_data = [{"input": question_maker(), "response": entry} for entry in grouped_sentences]
+    
+    # Write the structured dataset to a JSON file.
+    with open('../Datasets/cv_dataset.json', 'w', encoding='utf-8') as f:
+        json.dump(structured_data, f, ensure_ascii=False, indent=4)
+
+# Create the structured dataset
+process_questions()
