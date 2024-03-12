30821d3bab8fc1dacefd0cb3e2aa9ee403cfd9e7
Ali Alkhars <a-khars@hotmail.com>
2024-03-12 12:40:47 +0000

    Setup a json file to hold model outputs and evaluation results. Create a helper script to add outputs

---
+++ evaluators/output_insert_helper.py
@@ -0,0 +1,32 @@
+import json
+
+"""
+A helper script to make inserting model outputs to the
+evaluation_results.json file user friendly, so then the
+outputs could be easily evaluated.
+"""
+
+def insert_output(model_name, list_name):
+    file_name = '../Evaluation Datasets/evaluation_results.json'
+
+    # Read the existing data from the JSON file
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+
+    new_entry = [input("\nModel output: ")]
+
+    # Update the intended list
+    data[model_name][list_name].extend(new_entry)
+
+    # Write the updated data back to the JSON file
+    with open(file_name, 'w') as file:
+        json.dump(data, file, indent=4)
+
+    print(f"Updated the {list_name} of {model_name} successfully.")
+
+
+# Run the function
+model_name = "Llama-2-CareerBud"
+list_name = "auto_outputs"
+while True:
+    insert_output(model_name, list_name)
